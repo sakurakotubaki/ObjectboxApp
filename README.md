e3d0841dbfd146131ea25c7634c78b742f9f0a17
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2022-12-29 16:18:03 +0900

    update リアルタイムにデータを取得、idを指定する削除機能も追加

--- README.md
+++ README.md
@@ -9,11 +9,19 @@ ObjectBox は、オブジェクトを保持する超高速のモバイル デー
 [objectbox](https://pub.dev/packages/objectbox)
 [objectbox_flutter_libs](https://pub.dev/packages/objectbox_flutter_libs)
 [objectbox_generator](https://pub.dev/packages/objectbox_generator)
+[参考にした公式のサンプル](https://github.com/objectbox/objectbox-dart/tree/main/objectbox/example/flutter/event_management_tutorial/event_manager)
 
 ビルドランナーでファイルを生成するコマンド.
+もし、modelのファイルを移動したら、関係したファイルのimportしている場所が変化して、
+赤いエラーの原因になるので、もう一度コマンドを実行して、ファイルを自動生成する。
 ```
 flutter pub run build_runner build
 ```
+
+## 今回実装した機能
+1. 追加.
+2. idをして削除.
+3. 全てのデータを削除.
 
 ## 今回使用した技術
 objectboxに関係したpackageはversionを合わせる.
@@ -26,12 +34,17 @@ objectboxに関係したpackageはversionを合わせる.
 |objectbox_flutter_libs |^1.7.0    |
 |build_runner          |^2.3.3    |
 |objectbox_generator |^1.7.0     |
+|flutter_riverpod |^2.1.1     |
 
 -----
 
 ## アプリを作ることにした背景
 LocalDBをあまり使ったことがなく、objectboxが人気があり使いやすいことから、
 チュートリアルをやって勉強してみた。
+
+作成したアプリのversion3.0.0では、Riverpodへのリファクタリングを行いました。
+GitのTagで、version1、version2を作成して、どんなソースコードで作成していた
+かversionごとに記録を残しました。
 
 --------
 ## 感想
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:object_box_app/object_box.dart';
-import 'package:object_box_app/user.dart';
+import 'package:object_box_app/objectbox.g.dart';
+import 'package:object_box_app/model/user.dart';
 
+// TextEditingControllerを使うProvider
 final textProvider = StateProvider.autoDispose((ref) {
   // riverpodで使うには、('')が必要
   return TextEditingController(text: '');
 });
 // objectboxをインスタンス化して、DBにアクセスできるようにするProvider.
 final objectboxProvider = Provider((ref) => objectbox.store.box<User>());
+
+// StreamでListを使って、Userを型にしてモデルの情報を取得するProvider
+final objectStreamProvider = StreamProvider<List<User>>((ref) {
+  final builder = ref.watch(objectboxProvider).query()
+    ..order(User_.id, flags: Order.descending);
+  return builder.watch(triggerImmediately: true).map((query) => query.find());
+});
 
 /// アプリ全体を通してObjectBox Storeにアクセスできるようにします。
 late ObjectBox objectbox;
@@ -21,7 +30,6 @@ Future<void> main() async {
   objectbox = await ObjectBox.create();
 
   runApp(
-    // Adding ProviderScope enables Riverpod for the entire project
     const ProviderScope(child: MyApp()),
   );
 }
@@ -48,10 +56,12 @@ class DemoPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final boxProvider = ref.watch(objectboxProvider);
     final controllerProvider = ref.watch(textProvider);
+    final stream = ref.watch(objectStreamProvider);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ObjectBoxRiverpod2.1.1'),
+        backgroundColor: Colors.blueGrey,
+        title: const Text('ObjectBox'),
       ),
       body: Column(
         children: [
@@ -72,21 +82,37 @@ class DemoPage extends ConsumerWidget {
               child: Text('ユーザーを追加する')),
           const SizedBox(height: 16),
           ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
               onPressed: () {
-                // Userクラスを削除する.
+                // Userクラスを全て削除する.
                 boxProvider.removeAll();
               },
-              child: Text('ユーザーを削除する')),
+              child: Text('ユーザーを全て削除する')),
           const SizedBox(height: 16),
-          const Text('データ全体'),
-          Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              // objectboxからデータを取得して、mapメソッドで新しくリストを生成して、
-              // 画面に描画する.
-              children: boxProvider
-                  .getAll()
-                  .map((e) => Text('ID: ${e.id}, name: ${e.name ?? '名無し'}'))
-                  .toList()),
+          Expanded(
+              child: stream.when(
+            loading: () => const CircularProgressIndicator(),
+            error: (err, stack) => Text('Error: $err'),
+            data: (users) {
+              return ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    final user = users[index];
+                    return Card(
+                      child: ListTile(
+                        title: Text(user.name!),
+                        trailing: IconButton(
+                          color: Colors.red,
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {
+                            boxProvider.remove(user.id);
+                          },
+                        ),
+                      ),
+                    );
+                  });
+            },
+          ))
         ],
       ),
     );
